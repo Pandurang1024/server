4c33eb953b1eea47f328a55e48f1585292af6e7a
unknown <panduranga@ULTP_147.synerzipune.local>
2015-01-20 20:25:51 +0530

    added

---
+++ cookbooks/tomcat/files/default/installjava.sh
@@ -0,0 +1,4 @@
+sudo apt-get install software-properties-common python-software-properties -y
+sudo add-apt-repository ppa:webupd8team/java
+send "\n"
+sudo apt-get update
--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -7,6 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
+
+
+cookbook_file "/opt/installjava.sh" do
+  source "installjava.sh"
+  mode "0677"
+end
+
+bash "installjava" do
+  guard_interpreter :bash
+  code "/opt/installjava.sh"
+end
+
 package "tomcat7" do
 	action :install
 end
