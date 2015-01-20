c902cd3d2fe0f9afe36bf99a6c66b87fe327c3a8
unknown <panduranga@ULTP_147.synerzipune.local>
2015-01-20 21:10:33 +0530

    Added

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -15,3 +15,14 @@ end
 package "mysql-client" do
 action :install
 end
+
+execute 'createusr' do
+ cwd '/var'
+ command 'mysql -u root -e "CREATE USER 'rezoomex'@'localhost' IDENTIFIED BY 'password123'";'
+end
+
+
+execute 'createdb' do
+ cwd '/var'
+ command 'mysql -u root -e "create database rezoomex"; '
+end
